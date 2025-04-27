201ea258d8266c770b8baadd5527b26c0d19c807
Luka Urbanč <urbancluka3@gmail.com>
2025-04-27 14:12:18 +0200

    WIP holomorphic

--- src/holomorphic.rs
+++ src/holomorphic.rs
@@ -3,3 +3,31 @@
 // not yet sure if we should implement them just for dyadic complex numbers
 // implement derivate, integral (choose +C so that it maps 0 to 0)
 // implement methods eval, absolute value
+
+// Define the bounding sequences, used to limit the set of all sequences. By definition, the series (m_i)p^i converges for all p from [0, 1).
+// It may be necessary to expand this implementation, depending on needs. 
+
+// Call the Dyadic type from dyadic.rs: 
+use landau::dyadic::{Dyadic} ;
+
+struct BoundingSequences {
+    n_th: fn(u32) -> Dyadic,
+}
+
+impl BoundingSequences {
+    fn new(function: fn(u32) -> Dyadic) -> Self {
+        BoundingSequences {
+            n_th: function,
+        }
+    }
+}
+
+// Implement the sequences, used to identify complex holomorphic functions. 
+// By definition of any given sequence, the real an imaginary part of a_i are both bounded by m_i for from sequence m_n of the type BoundingSequence. 
+struct HolomorphicSequence {
+    n_th : fn(u32) -> Dyadic
+}
+
+struct ComplexFunction {
+
+}
