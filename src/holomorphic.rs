3c5eb478b0bf3937a0f9314785dc44e044f72e33
Matija Derganc <matijaderganc@Matijas-MacBook-Air.local>
2025-04-30 00:19:33 +0200

    added grids, graphics

--- src/holomorphic.rs
+++ src/holomorphic.rs
@@ -1,5 +1,20 @@
-// TODO: 
-// implement holomorphic funtions as vectors, have addition, subtraction, mult (not sure if needed)
-// not yet sure if we should implement them just for dyadic complex numbers
-// implement derivate, integral (choose +C so that it maps 0 to 0)
-// implement methods eval, absolute value
+use crate::dyadic::{Dyadic, ComplexDyadic, Interval, psi} ;
+
+pub struct Holomorphic {
+    pub coef : Vec<ComplexDyadic> 
+}
+impl Holomorphic {
+    pub fn new(v : Vec<ComplexDyadic>) -> Self {
+        return Holomorphic{coef : v}
+    }
+
+    pub fn eval(&self, z : ComplexDyadic) -> ComplexDyadic {
+        let mut result = ComplexDyadic::zero();
+
+        for (k, coeff) in self.coef.iter().enumerate() {
+            let zk = z.powi(k as i32);
+            result = result + (*coeff * zk);
+        }
+        result
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,4 @@
 pub mod dyadic;
 pub mod psi ;
+pub mod holomorphic;
+pub mod covering_grids;
