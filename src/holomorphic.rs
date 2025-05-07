bad4beb73613005456f561d945890359b756901a
Luka Urbanč <urbancluka3@gmail.com>
2025-05-07 21:00:41 +0200

    Add derivative for ComplexFunction

--- src/holomorphic.rs
+++ src/holomorphic.rs
@@ -1,123 +1,92 @@
-// TODO: 
+// TODO:
 // - implement holomorphic mult (not sure if needed)
 // - not yet sure if we should implement them just for dyadic complex numbers. This may need to be revised later on. Functions now work only on ComplexDyadic
 // - implement derivate, integral (choose +C so that it maps 0 to 0) -> This is the most important aspect of this struct, as it will be used later on
 // - do some basic testing to see function return expected values
 
-// Call the Dyadic type from dyadic.rs: 
-use crate::dyadic::{ComplexDyadic, Dyadic, add_vec, add_complex_vec, sub_complex_vec} ;
-use std::ops::{Add, Mul, Sub, Div}; //division not yet implemented
+// Call the Dyadic type from dyadic.rs:
+use crate::dyadic::{add_complex_vec, add_vec, sub_complex_vec, ComplexDyadic, Dyadic};
+use std::ops::{Add, Bound, Div, Mul, Sub}; //division not yet implemented
 use std::sync::Arc;
+use std::vec;
 
-// Function to map vector to function which returns n-th element. 
+// Function to map vector to function which returns n-th element.
 pub fn vec_to_sequence(vec: &Vec<Dyadic>) -> impl Fn(u32) -> Dyadic + '_ {
     move |n: u32| {
-        vec.get(n as usize)          // safe, no panic
-            .cloned()                 // use `copied()` if Dyadic implements Copy
+        vec.get(n as usize) // safe, no panic
+            .cloned() // use `copied()` if Dyadic implements Copy
             .unwrap_or_else(Dyadic::zero)
     }
 }
 
 pub fn comp_vec_to_sequence(vec: &Vec<ComplexDyadic>) -> impl Fn(u32) -> ComplexDyadic + '_ {
     move |n: u32| {
-        vec.get(n as usize)          
-            .cloned()                 
+        vec.get(n as usize)
+            .cloned()
             .unwrap_or_else(ComplexDyadic::zero)
     }
 }
 // Define the bounding sequences, used to limit the set of all sequences. By definition, the series (m_i)p^i converges for all p from [0, 1).
-// It may be necessary to expand this implementation, depending on needs. 
+// It may be necessary to expand this implementation, depending on needs.
 #[derive(Clone)]
 pub struct BoundingSequence {
-    n_th: Arc<dyn Fn(u32) -> Dyadic>,
-    vector : Vec<Dyadic>,
+    vector: Vec<Dyadic>,
 }
 
 impl Add for BoundingSequence {
     type Output = Self;
 
     fn add(self, other: Self) -> Self {
-        let left  = self.n_th;
-        let right = other.n_th;
-
         Self {
-            n_th: Arc::new(move |n| {
-                let a = left(n);
-                let b = right(n);
-                if a > b { a } else { b }
-            }
-        ),
-        vector: add_vec(&self.vector, &other.vector),
+            vector: add_vec(&self.vector, &other.vector),
         }
     }
 }
-
 
 impl BoundingSequence {
-    pub fn new<F>(f: F, vector : Vec<Dyadic>) -> Self
-    where
-        F: Fn(u32) -> Dyadic + Send + Sync + 'static,
-    {
-        Self {
-            n_th: Arc::new(f),
-            vector: vector, // Initialize the vector as an empty vector
-        }
+    pub fn new(vector: Vec<Dyadic>) -> BoundingSequence {
+        BoundingSequence { vector: vector }
     }
 }
-// Implement the sequences, used to identify complex holomorphic functions. 
-// By definition of any given sequence, the real an imaginary part of a_i are both bounded by m_i for a sequence m_n of the type BoundingSequence. 
+// Implement the sequences, used to identify complex holomorphic functions.
+// By definition of any given sequence, the real an imaginary part of a_i are both bounded by m_i for a sequence m_n of the type BoundingSequence.
 #[derive(Clone)]
 pub struct ExpansionCoefficients {
-    n_th : Arc<dyn Fn(u32) -> ComplexDyadic>,
-    vector : Vec<ComplexDyadic>,
+    vector: Vec<ComplexDyadic>,
 }
 
 impl Add for ExpansionCoefficients {
     type Output = Self;
 
     fn add(self, other: Self) -> Self {
-        let left  = self.n_th;
-        let right = other.n_th;
-
-        Self {
-            n_th: Arc::new(move |n| left(n) + right(n)),
-            vector: add_complex_vec(&self.vector, &other.vector)
+        ExpansionCoefficients {
+            vector: add_complex_vec(&self.vector, &other.vector),
         }
     }
 }
 
 impl Sub for ExpansionCoefficients {
-    type Output = Self; 
+    type Output = Self;
 
-    fn sub(self, other : Self) -> Self {
-        let left = self.n_th;
-        let right = other.n_th;
-        Self {
-            n_th: Arc::new(move |n| left(n) - right(n)),
+    fn sub(self, other: Self) -> ExpansionCoefficients {
+        ExpansionCoefficients {
             vector: sub_complex_vec(&self.vector, &other.vector),
         }
     }
 }
 
 impl ExpansionCoefficients {
-    pub fn new<F>(f: F, vector : Vec<ComplexDyadic>) -> Self
-    where
-        F: Fn(u32) -> ComplexDyadic + Send + Sync + 'static,
-    {
-        Self { 
-            n_th: Arc::new(f), 
-            vector: vector, 
-        }
+    pub fn new(vector: Vec<ComplexDyadic>) -> ExpansionCoefficients {
+        ExpansionCoefficients { vector: vector }
     }
 }
 
 // A given holomorphic function consists of a bounding sequence (type BoundingSequence) and a bounded sequence, which represents the coefficients in its expansion (type ExpansionCoefficients)
-// Functions are (for now) defined only for ComplexDyadic numbers, not arbitrary complex numbers. This could need to be changed later on. 
+// Functions are (for now) defined only for ComplexDyadic numbers, not arbitrary complex numbers. This could need to be changed later on.
 pub struct ComplexFunction {
     bounding_sequence: BoundingSequence,
     expansion_coefficients: ExpansionCoefficients,
-    function: Box<dyn Fn(ComplexDyadic) -> ComplexDyadic>,
-    // Upper limit defaults to 100, but a separate constructor if defined 
+    // Upper limit defaults to 100, but a separate constructor if defined
     upper_limit_of_summation: u32,
 }
 
@@ -126,24 +95,14 @@ impl ComplexFunction {
     pub fn new(
         bounding_sequence: BoundingSequence,
         expansion_coefficients: ExpansionCoefficients,
-    ) -> Self {
-        let nth_fn = expansion_coefficients.n_th.clone();
-
+    ) -> ComplexFunction {
         let len = expansion_coefficients.vector.len();
         let len = len as u32;
 
-        let func = move |z: ComplexDyadic| {
-                    let mut sum = ComplexDyadic::zero();
-                    for i in 1..=len {
-                        sum = sum + nth_fn(i) * z.powi(i as i32);
-                    }
-                    ComplexDyadic::one() + sum
-                };
-        Self{
-            bounding_sequence: bounding_sequence, 
+        ComplexFunction {
+            bounding_sequence: bounding_sequence,
             expansion_coefficients: expansion_coefficients,
-            function: Box::new(func),
-            upper_limit_of_summation: len
+            upper_limit_of_summation: len,
         }
     }
     // pub fn new_with_upper_limit(
@@ -170,46 +129,49 @@ impl ComplexFunction {
     // }
 
     pub fn eval(&self, z: ComplexDyadic) -> ComplexDyadic {
-        (self.function)(z)
+        let mut sum = ComplexDyadic::zero();
+        for i in 1..=self.upper_limit_of_summation {
+            sum = sum + self.expansion_coefficients.vector[i as usize] * z.powi(i as i32)
+        }
+        return sum + ComplexDyadic::one();
     }
 
-    // This could need to be checked if it works properly. 
+    // This could need to be checked if it works properly.
     pub fn derivative(&self) -> ComplexFunction {
         let coefficients = &self.expansion_coefficients.vector;
         let length = coefficients.len();
         let mut derivative_coefficients: Vec<ComplexDyadic> = Vec::with_capacity(length - 1);
-        
+
         // Calculate the derivative coefficients
         for i in 1..length {
-            derivative_coefficients.push(coefficients[i] * ComplexDyadic::new(Dyadic::new(i as i128, 0), Dyadic::zero()));
+            derivative_coefficients.push(
+                coefficients[i] * ComplexDyadic::new(Dyadic::new(i as i128, 0), Dyadic::zero()),
+            );
         }
-    
-        let n_th = comp_vec_to_sequence(&derivative_coefficients);
-    
         ComplexFunction::new(
-            self.bounding_sequence.clone(), 
-            ExpansionCoefficients::new(n_th, derivative_coefficients)
+            self.bounding_sequence.clone(),
+            ExpansionCoefficients::new(derivative_coefficients),
         )
     }
 }
 
-impl Add for ComplexFunction { 
+impl Add for ComplexFunction {
     type Output = ComplexFunction;
-    fn add(self, other : ComplexFunction) -> ComplexFunction {
+    fn add(self, other: ComplexFunction) -> ComplexFunction {
         ComplexFunction::new(
             self.bounding_sequence + other.bounding_sequence,
-            self.expansion_coefficients + other.expansion_coefficients
-            )    
+            self.expansion_coefficients + other.expansion_coefficients,
+        )
     }
 }
 
 impl Sub for ComplexFunction {
     type Output = Self;
 
-    fn sub(self, other : Self) -> Self {
+    fn sub(self, other: Self) -> Self {
         Self::new(
-            self.bounding_sequence + other.bounding_sequence, 
-            self.expansion_coefficients - other.expansion_coefficients
+            self.bounding_sequence + other.bounding_sequence,
+            self.expansion_coefficients - other.expansion_coefficients,
         )
     }
 }
